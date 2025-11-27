708691fa077248074a151b46234da3ad456d5a42
Hayat Khan <hayatkhan2024@uop.edu.pk>
2025-11-27 15:31:05 +0500

    The journey begins

---
+++ src/config.py
@@ -0,0 +1,21 @@
+# src/config.py
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+DATA_DIR = os.path.join(BASE_DIR, "data")
+IMAGES_DIR = os.path.join(DATA_DIR, "images")
+MASKS_DIR = os.path.join(DATA_DIR, "masks")
+VECTOR_DIR = os.path.join(DATA_DIR, "vectors")
+RESULTS_DIR = os.path.join(DATA_DIR, "results")
+
+# SAM
+SAM_CHECKPOINT = os.path.join(BASE_DIR, "models", "sam_vit_h.pth")
+SAM_MODEL_TYPE = "vit_h"
+
+# Processing settings
+SLIC_SEGMENTS = 70     # can tune later
+NDVI_THRESHOLD = 0.2   # weak vegetation mask
+
+
---
+++ src/data_loader.py
@@ -0,0 +1,31 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+
+import xarray as xr
+import rasterio
+import numpy as np
+from src.config import *
+
+
+def load_nc_image(path):
+    ds = xr.open_dataset(path)
+    return ds
+
+def build_rgb_from_nc(ds, t=0):
+    B2 = ds["B2"].isel(time=t).values.astype(np.float32)
+    B3 = ds["B3"].isel(time=t).values.astype(np.float32)
+    B4 = ds["B4"].isel(time=t).values.astype(np.float32)
+
+    rgb = np.dstack((B4, B3, B2))
+    rgb_min, rgb_max = np.percentile(rgb, (2, 98))
+    rgb_norm = (rgb - rgb_min) / (rgb_max - rgb_min)
+    rgb_norm = np.clip(rgb_norm, 0, 1)
+
+    return rgb_norm
+
+def load_mask(path):
+    with rasterio.open(path) as src:
+        mask = src.read(1)
+    return mask
+
